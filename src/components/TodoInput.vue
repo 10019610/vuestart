8ea0b11313dfe4865e5b0a6d8427b4e1f375ea1f
young jo <a10019610@gmail.com>
2022-03-02 00:51:09 +0900

    first git push

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="fa-regular fa-calendar-plus addBtn"></i>
+       <!-- 버전이 안맞는건지 아이콘이 안뜸 해결못해서 넘어감 -->
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  height: 40px;
+  width: 80%;
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478F8, #8763FB);
+  display: 3rem;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  width: 50px;
+  vertical-align: middle;
+}
+</style>
